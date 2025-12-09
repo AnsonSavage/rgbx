a1b819208e8de2b477b6483d986c77efd935ffe5
AnsonSavage <ansonsavage@gmail.com>
2025-12-09 15:19:25 -0700

    Add support for multi-GPU training. I'm not sure if the export PYTHONPATH bit is necessary or not... I think I accidentally vibe-coded an update to one of the huggingface packages, requiring it to look to a different version in my home directory. This is yet to be tested.

--- x2rgb/run_train.sh
+++ x2rgb/run_train.sh
@@ -17,10 +17,7 @@ DATASET_TYPE="discrete"  # Options: "discrete" or "hdri"
 MODEL_NAME="zheng95z/x-to-rgb"
 OUTPUT_DIR="x2rgb-finetuned_${DATASET_TYPE}_with_velocity"
 export HF_HUB_OFFLINE=1
-
-# Absolute path to the conda environment created in this repo
-CONDA_ENV_PATH="/grphome/grp_cs_650_rgb_x/conda_env"
-conda activate "${CONDA_ENV_PATH}"
+export PYTHONPATH=/home/ansonsav/.local/lib/python3.10/site-packages:$PYTHONPATH
 
 # Move to training directory
 cd /grphome/grp_cs_650_rgb_x/rgbx/x2rgb/
@@ -30,7 +27,17 @@ cd /grphome/grp_cs_650_rgb_x/rgbx/x2rgb/
 
 # Recommended: configure accelerate once interactively on the machine with `accelerate config`
 # Then run via accelerate launch so it handles distributed setup automatically.
-accelerate launch train_x2rgb.py \
+
+# GPU Selection
+# We rely on SLURM to set CUDA_VISIBLE_DEVICES.
+if [ -n "$SLURM_GPUS_ON_NODE" ]; then
+    NUM_GPUS=$SLURM_GPUS_ON_NODE
+else
+    # Fallback: count commas in CUDA_VISIBLE_DEVICES
+    NUM_GPUS=$(echo $CUDA_VISIBLE_DEVICES | tr ',' '\n' | wc -l)
+fi
+
+/grphome/grp_cs_650_rgb_x/conda_env/bin/accelerate launch --num_processes=$NUM_GPUS train_x2rgb.py \
   --pretrained_model_name_or_path="$MODEL_NAME" \
   --dataset_type="$DATASET_TYPE" \
   --output_dir="$OUTPUT_DIR" \
